d259dd9b55e570bcafa769f212623a18e9ee2d37
Blancher <108644584+Blancher@users.noreply.github.com>
2022-12-23 09:15:00 -0800

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,12 @@
 const addTask = document.getElementById('add-task');
 const taskContainer = document.getElementById('task-container');
 const inputTask = document.getElementById('input-task');
+let counter = 0;
 
 // Event listener for add button
 
 addTask.addEventListener('click', function() {
+    counter += 1;
     let task = document.createElement('div');
     task.classList.add('task');
     let li = document.createElement('li');
@@ -31,5 +33,10 @@ addTask.addEventListener('click', function() {
     })
     deleteButton.addEventListener('click', function() {
         deleteButton.parentElement.remove();
+        if (counter > 0) {
+            counter -= 1;
+        }
+        document.getElementById('length').textContent = counter;
     })
+    document.getElementById('length').textContent = counter;
 });
